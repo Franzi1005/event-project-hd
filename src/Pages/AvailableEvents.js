2104355da63edfb517d6692061e80857e16227b8
Franzi1005 <fschallhorn@gmail.com>
2023-06-14 17:11:39 +0200

    added navigate to the attend button

--- src/Pages/AvailableEvents.js
+++ src/Pages/AvailableEvents.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AvailableEvents() {
+  let navigate = useNavigate();
   return (
     <div>
       <h1>Drinks @Friedrich</h1>
@@ -8,7 +10,9 @@ export default function AvailableEvents() {
 
       <h5>Date: 12.06.2023</h5>
       <h5>Time: 08:00 pm</h5>
-      <button className="btn btn-primary">Attend</button>
+      <button className="btn btn-primary" onClick={() => navigate("/signUp")}>
+        Attend
+      </button>
     </div>
   );
 }
